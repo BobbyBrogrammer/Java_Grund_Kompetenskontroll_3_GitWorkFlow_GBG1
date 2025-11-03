f363369c44ccc2e8f5b54ae504c52384b0599b84
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-11-03 10:12:09 +0100

    Koppla till Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.service.CompletionService;
+
+
 public class Main {
     public static void main(String[] args) {
+            CompletionService completionService = new CompletionService();
 
-
-
+            completionService.completeProcess(
+                    "kund@example.com",
+                    "ABC123",
+                    "car"
+            );
+        }
     }
-}
