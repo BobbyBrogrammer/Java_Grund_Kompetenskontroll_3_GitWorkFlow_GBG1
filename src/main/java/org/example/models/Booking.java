437974f0392617957fbe5df1b5da7795b5b0ab8a
AdnanZasella <adnanzasella@hotmail.com>
2025-11-03 10:04:37 +0100

    Adnan Person 1 Domain

--- src/main/java/org/example/models/Booking.java
+++ src/main/java/org/example/models/Booking.java
@@ -1,4 +1,28 @@
 package org.example.models;
 
-public class Booking {
-}
+import java.io.ObjectInputFilter;
+import java.time.LocalDate;
+
+    public class Booking {
+
+        private static int counter = 1;
+        private int id;
+        private Vehicle vehicle;
+        private LocalDate date;
+        private double price;
+        private String customerEmail;
+        private Status status;
+        private BookingType bookingType;
+
+
+        public Booking(Vehicle vehicle, LocalDate date, double price, String customerEmail, BookingType bookingType) {
+            this.id = counter++;
+            this.vehicle = vehicle;
+            this.date = date;
+            this.price = price;
+            this.customerEmail = customerEmail;
+            this.bookingType = bookingType;
+            this.status = Status.NOT_DONE;
+        }
+    }
+
--- src/main/java/org/example/models/RepairBooking.java
+++ src/main/java/org/example/models/RepairBooking.java
@@ -1,4 +1,25 @@
 package org.example.models;
 
-public class RepairBooking {
+import java.time.LocalDate;
+
+public class RepairBooking extends Booking {
+    private String actionDescription; // Vad som ska repareras
+
+    public RepairBooking(Vehicle vehicle, LocalDate date, double price, String customerEmail, String actionDescription) {
+        super(vehicle, date, price, customerEmail, BookingType.REPAIR);
+        this.actionDescription = actionDescription;
+    }
+
+    public String getActionDescription() {
+        return actionDescription;
+    }
+
+    public void setActionDescription(String actionDescription) {
+        this.actionDescription = actionDescription;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Action: " + actionDescription;
+    }
 }
