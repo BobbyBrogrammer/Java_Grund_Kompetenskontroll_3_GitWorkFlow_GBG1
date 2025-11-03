84d8bfcb4e8a4bc4b4f49e9451af57723b657f6e
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-03 15:23:53 +0100

    commit bug ^^

--- src/main/java/org/example/service/PriceService.java
+++ src/main/java/org/example/service/PriceService.java
@@ -1,18 +1,60 @@
 package org.example.service;
 
+import org.example.models.BookingType;
+import org.example.models.Vehicle;
+
 public class PriceService {
-    public double calculatePrice(String vehicleType) {
-        // Enkel prisberäkning baserat på typ (exempel-tabell)
-        switch (vehicleType.toLowerCase()) {
-            case "car":
-                return 500.0;
-            case "truck":
-                return 800.0;
-            case "motorcycle":
-                return 300.0;
-            default:
-                System.out.println("Okänd fordonstyp, pris sätts till 0 kr.");
-                return 0.0;
-        }
+    private static final double INSPECTION_PRICE = 550.0;
+
+//    public double calculatePrice(String vehicleType) {
+//        // Enkel prisberäkning baserat på typ (exempel-tabell)
+//        switch (vehicleType.toLowerCase()) {
+//            case "car":
+//                return 500.0;
+//            case "truck":
+//                return 800.0;
+//            case "motorcycle":
+//                return 300.0;
+//            default:
+//                System.out.println("Okänd fordonstyp, pris sätts till 0 kr.");
+//                return 0.0;
+//        }
+//    }
+
+        public double calculatePrice(BookingType bookingType, Vehicle vehicle) {
+        return switch (bookingType) {
+            case INSPECTION -> getInspectionPrice();
+            case SERVICE -> calculateServicePrice(vehicle.getYearModel());
+            case REPAIR -> 0.0;
+        };
     }
+
+        public double getInspectionPrice() {
+        return INSPECTION_PRICE;
+        }
+
+        public double calculateServicePrice(int yearModel) {
+            if (yearModel > 2020) {
+                return 1500.0;
+            } else if (yearModel >= 2015) {
+                return 1800.0;
+            } else if (yearModel >= 2010) {
+                return 2000.0;
+            } else if (yearModel >= 2005) {
+                return 2300.0;
+            } else {
+                return 2800.0;
+            }
+        }
+
+        public double confirmRepairPrice(double repairPrice) {
+            if (repairPrice <= 0) {
+                throw new IllegalArgumentException("Priset för reparation måste vara större än 0 kr.");
+            }
+            return repairPrice;
+        }
+
+
+
+
 }
