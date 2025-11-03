20a60eb35e6a77404d369df4d9bbe791c5c92ee5
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-03 10:43:11 +0100

    RepairBooking commit "Customer fix"

--- src/main/java/org/example/models/RepairBooking.java
+++ src/main/java/org/example/models/RepairBooking.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class RepairBooking extends Booking {
     private String actionDescription; // Vad som ska repareras
 
-    public RepairBooking(Vehicle vehicle, LocalDate date, double price, String customerEmail, String actionDescription) {
-        super(vehicle, date, price, customerEmail, BookingType.REPAIR);
+    public RepairBooking(Vehicle vehicle, LocalDate date, double price, Customer customer, String actionDescription) {
+        super(vehicle, date, price, customer, BookingType.REPAIR);
         this.actionDescription = actionDescription;
     }
 
