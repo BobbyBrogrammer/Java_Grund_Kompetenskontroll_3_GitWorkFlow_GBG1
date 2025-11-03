f26d7526375defd4b9fa2ea80d1c5dc04cf06c15
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-03 14:55:31 +0100

    commit bookingservice, loggingservice,validationservice small fix(temporary fix)

--- src/main/java/org/example/repository/BookingRepository.java
+++ src/main/java/org/example/repository/BookingRepository.java
@@ -2,16 +2,11 @@ package org.example.repository;
 
 import org.example.models.Booking;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public abstract class BookingRepository implements Repository {
     Map<UUID, Booking> vehicle = new LinkedHashMap<>();
-
     public BookingRepository(Map<UUID, Booking> vehicle) {
         this.vehicle = vehicle;
     }
-
-
 }
