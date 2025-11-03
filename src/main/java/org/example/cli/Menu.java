d54f3671d4a831955f81bedfb318759b1bbc4ac5
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-03 22:10:48 +0100

    commit CLI big update Robin

--- src/main/java/org/example/cli/Menu.java
+++ src/main/java/org/example/cli/Menu.java
@@ -1,53 +1,34 @@
 package org.example.cli;
 
+import org.example.service.CompletionService;
 import org.example.systemIO.IIO;
-import org.example.systemIO.SystemIO;
 
 public class Menu {
     private final IIO io;
+    private final OutputHandler output;
     private final MenuMethods menuMethods;
 
-    public Menu() {
-        this.io = new SystemIO();
-        this.menuMethods = new MenuMethods(io);
+    public Menu(IIO io, InputHandler input, OutputHandler output, CompletionService completionService) {
+        this.io = io;
+        this.output = output;
+        this.menuMethods = new MenuMethods(io, input, output, completionService);
     }
 
     public void showMainMenu() {
-        int choice;
-        do {
-            io.printLine("=== Bilmeckarna AB ===");
-            io.printLine("1. Skapa ny bokning");
-            io.printLine("2. Visa alla bokningar");
-            io.printLine("3. Sök bokning");
-            io.printLine("4. Ta bort bokning");
-            io.printLine("5. Uppdatera bokning");
-            io.printLine("0. Avsluta");
-            io.printLine("Välj ett alternativ: ");
-
-            // Läs in val
-            String input = io.readLine();
-            choice = parseInt(input);
+        while (true) {
+            output.printMenyChoices();
+            String choice = io.readLine().trim();
 
             switch (choice) {
-                case 1 -> menuMethods.createBooking();
-                case 2 -> menuMethods.showAllBookings();
-                case 3 -> menuMethods.searchBooking();
-                case 4 -> menuMethods.deleteBooking();
-                case 5 -> menuMethods.updateBooking();
-                case 0 -> io.printLine("Avslutar programmet. Hej då!");
-                default -> io.printLine("Ogiltigt val, försök igen!");
+                case "1" -> menuMethods.createBooking();
+                case "2" -> menuMethods.showAllBookings();
+                case "3" -> menuMethods.searchBooking();
+                case "4" -> menuMethods.deleteBooking();
+                case "5" -> menuMethods.updateBooking();
+                case "0" -> output.printExitProgram();
+                default -> output.printDefaultMenuChoice();
             }
-
-            io.printLine("");
-
-        } while (choice != 0);
-    }
-
-    private int parseInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            return -1;
         }
     }
 }
+
