503ebd085cc5f68f8d6446da68f23e8f5c92ecf9
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-11-03 09:49:54 +0100

    Skapa PriceCalculator

--- src/main/java/org/example/service/PriceCalculatorService.java
+++ src/main/java/org/example/service/PriceCalculatorService.java
@@ -1,4 +1,17 @@
 package org.example.service;
 
 public class PriceCalculatorService {
+    public double calculateService(String vehicleType) {
+        switch (vehicleType.toLowerCase()) {
+            case "car":
+                return 500.0;
+            case "truck":
+                return 800.0;
+            case "motorcycle":
+                return 300.0;
+            default:
+                System.out.println("Okänd fordonstyp, pris sätts till 0 kr.");
+                return 0.0;
+        }
+    }
 }
