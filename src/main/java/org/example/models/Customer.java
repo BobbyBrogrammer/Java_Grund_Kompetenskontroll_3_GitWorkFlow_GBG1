8019a16924e4be225ec6905fa9b40fc8209e6f72
AdnanZasella <adnanzasella@hotmail.com>
2025-11-03 10:16:53 +0100

    Adnan person1 Customer class added and Booking Class adapted with Customer Class

--- src/main/java/org/example/models/Customer.java
+++ src/main/java/org/example/models/Customer.java
@@ -2,9 +2,9 @@ package org.example.models;
 
 public class Customer {
 
-    private String name;          // Kundens namn
-    private String phoneNumber;   // Telefonnummer
-    private String email;         // E-postadress (valideras i ValidationService)
+    private String name;
+    private String phoneNumber;
+    private String email;        
 
     public Customer(String name, String phoneNumber, String email) {
         this.name = name;
