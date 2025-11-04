276a71833fb8a2e6fe3c0d75d5753f7a6b6bfc80
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-11-04 10:33:15 +0100

    skapa en för meny

--- src/main/java/org/example/app/StartApp.java
+++ src/main/java/org/example/app/StartApp.java
@@ -6,6 +6,6 @@ public class StartApp {
     public static void main(String[] args) {
            AppConfig app = new AppConfig();
            Menu menu = app.menuRunner();
-           menu.showMainMenu();
+           menu.showServiceMenu();
     }
 }
