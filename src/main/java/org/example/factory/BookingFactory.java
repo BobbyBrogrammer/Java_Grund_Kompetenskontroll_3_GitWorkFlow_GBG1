4b34f45c8a5ec8e53f40c6c02801dbcc367adfa4
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-04 19:11:16 +0100

    commit Booking,BookingFactory,BookingService,ConsoleUI, PriceService. ADDED calculateServicePrice method

--- src/main/java/org/example/factory/BookingFactory.java
+++ src/main/java/org/example/factory/BookingFactory.java
@@ -1,6 +1,7 @@
 package org.example.factory;
 
 import org.example.models.Booking;
+import org.example.models.BookingType;
 import org.example.models.Customer;
 import org.example.models.Vehicle;
 import org.example.service.PriceService;
@@ -18,6 +19,11 @@ public class BookingFactory {
 
     public Booking bookInspection(Vehicle vehicle, LocalDate localDate, Customer customer){
         double price = priceService.bookInspection();
-        return new Booking(vehicle, localDate, price, customer);
+        return new Booking(vehicle, localDate, price, customer, BookingType.INSPECTION);
     }
+
+    public Booking bookService(Vehicle vehicle, LocalDate date, Customer customer, double price) {
+        return new Booking(vehicle, date, price, customer, BookingType.SERVICE);
+    }
+
 }
