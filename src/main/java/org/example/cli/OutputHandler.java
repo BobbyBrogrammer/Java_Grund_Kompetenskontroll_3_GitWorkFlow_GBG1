e948ef0dd68479b5c8c7a436a34f3a5f2a12d33c
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-04 14:30:13 +0100

    commit fixed Menu i printMenyChoices

--- src/main/java/org/example/cli/OutputHandler.java
+++ src/main/java/org/example/cli/OutputHandler.java
@@ -104,6 +104,7 @@ public class OutputHandler {
         io.printLine("3. Sök bokning");
         io.printLine("4. Uppdatera bokning");
         io.printLine("5. Ta bort bokning");
+        io.printLine("6. Tillbaka till huvudmenyn");
         io.printLine("0. Avsluta");
     }
     public void printMenyChoicesRepair() {
