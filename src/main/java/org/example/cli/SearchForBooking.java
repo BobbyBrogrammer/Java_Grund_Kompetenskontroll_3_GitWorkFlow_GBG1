b27ecb59800f710bcdac0b8656fb992610f2a8d5
alexandersson98 <alexandersson98l@gmail.com>
2025-11-05 19:45:56 +0100

    logging

--- src/main/java/org/example/cli/SearchForBooking.java
+++ src/main/java/org/example/cli/SearchForBooking.java
@@ -1,19 +1,24 @@
 package org.example.cli;
 
+import org.example.service.LoggingService;
+
 import java.util.List;
 
 public class SearchForBooking {
     private final InputHandler input;
     private final OutputHandler output;
+    private final LoggingService logger;
 
-    public SearchForBooking(InputHandler input, OutputHandler output) {
+    public SearchForBooking(InputHandler input, OutputHandler output, LoggingService logger) {
         this.input = input;
         this.output = output;
+        this.logger = logger;
     }
 
     public void searchBooking(List<String> bookings) {
         output.printSearchForBookingTitle();
         String search = input.readRegistrationNumber();
+        logger.logInfo("Sökning av registreringsnummer startad " + search);
 
         List<String> bookingFound = bookings.stream()
                 .filter(b -> b.contains(search))
@@ -21,9 +26,11 @@ public class SearchForBooking {
 
         if (bookingFound.isEmpty()) {
             output.printNoBookingFound();
+            logger.logInfo("Inga bokningar med detta registreringsnummer hittades" + search);
         } else {
             output.printBookingFound();
             bookingFound.forEach(output::printSuccess);
+            logger.logInfo("Hittad " + bookingFound.size() + "bokning(s) för registreringsnummer " + search);
         }
     }
 }
--- src/main/java/org/example/cli/UpdateBooking.java
+++ src/main/java/org/example/cli/UpdateBooking.java
@@ -1,19 +1,24 @@
 package org.example.cli;
 
+import org.example.service.LoggingService;
+
 import java.util.List;
 
 public class UpdateBooking {
     private final InputHandler input;
     private final OutputHandler output;
+    private final LoggingService logger;
 
-    public UpdateBooking(InputHandler input, OutputHandler output) {
+    public UpdateBooking(InputHandler input, OutputHandler output, LoggingService logger) {
         this.input = input;
         this.output = output;
+        this.logger = logger;
     }
 
     public void updateBooking(List<String> bookings) {
         output.printUpdateBookTitle();
         String search = input.readRegistrationNumber();
+        logger.logInfo("Uppdatera bokningar startad för registreringsnummer " + search);
 
         bookings.stream()
                 .filter(b -> b.contains(search))
@@ -24,7 +29,11 @@ public class UpdateBooking {
                     int index = bookings.indexOf(foundBooking);
                     bookings.set(index, search + " (" + newVehicle + ")");
                     output.printBookingHaveBeenUpdated();
-                }, () -> output.printNoBookingFoundToUpdate());
+                    logger.logInfo("Bokning uppdaterad för registreringsnummer " + search);
+                }, () -> {
+                    output.printNoBookingFoundToUpdate();
+                    logger.logInfo("Inge bokning hittad för att uppdatera registreringsnummer " + search);
+                });
         }
     }
 
