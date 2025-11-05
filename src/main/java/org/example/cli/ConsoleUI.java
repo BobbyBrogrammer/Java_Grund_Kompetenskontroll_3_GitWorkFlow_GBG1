690402090b969250d17edc6881b2dc4c6800b200
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-11-05 21:20:29 +0100

    commit fixed solid in ConsoleUI

--- src/main/java/org/example/cli/ConsoleUI.java
+++ src/main/java/org/example/cli/ConsoleUI.java
@@ -146,15 +146,18 @@ public class ConsoleUI implements BookingUI{
                 input.readRegistrationNumber(),
                 input.readVehicleModel(),
                 input.readYearModel());
+        vehicleRepository.add(vehicle);
         //Skapa kund
         Customer customer = customerFactory.createCustomer(
                 input.readCustomerName(),
                 input.readPhoneNumber(),
                 input.readEmail());
+        customerRepository.add(customer);
         //Läs in datum
         LocalDate date = input.readDate();
         //Skapa bokning
         Booking booking = bookingService.createBooking(vehicle, date, customer, BookingType.SERVICE);
+        bookingRepository.add(booking);
         //Visa resultat
         if (booking != null) {
             output.printSuccess("Service bokning skapad!\n" + booking);
@@ -171,11 +174,13 @@ public class ConsoleUI implements BookingUI{
                 input.readRegistrationNumber(),
                 input.readVehicleModel(),
                 input.readYearModel());
+        vehicleRepository.add(vehicle);
         //Skapa kund
         Customer customer = customerFactory.createCustomer(
                 input.readCustomerName(),
                 input.readPhoneNumber(),
                 input.readEmail());
+        customerRepository.add(customer);
         //Läs in datum
         LocalDate date = input.readDate();
         //Skapa bokning
