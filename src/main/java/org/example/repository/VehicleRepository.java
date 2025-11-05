4b01586139f7a9516e5f76d6c0ca89e1039ff62f
alexandersson98 <alexandersson98l@gmail.com>
2025-11-06 00:52:10 +0100

    logger

--- src/main/java/org/example/repository/VehicleRepository.java
+++ src/main/java/org/example/repository/VehicleRepository.java
@@ -1,30 +1,42 @@
 package org.example.repository;
 
 import org.example.models.Vehicle;
+import org.example.service.LoggingService;
+
 import java.util.*;
 
 public class VehicleRepository implements Repository<Vehicle, String> {
     private final Map<String, Vehicle> vehicles = new LinkedHashMap<>();
+    private final LoggingService logger;
 
+    public VehicleRepository(LoggingService logger) {
+        this.logger = logger;
+    }
 
     @Override
     public void add(Vehicle v){
         vehicles.put(v.getRegistrationNumber(), v);
+        logger.logInfo("Lade till fordon med reg.nr: " + v.getRegistrationNumber());
     }
 
     @Override
     public Optional<Vehicle> findById(String plate){
-        return Optional.ofNullable(vehicles.get(plate));
+        Optional<Vehicle>allVehicles = Optional.ofNullable(vehicles.get(plate));
+        logger.logInfo("Hämtade alla fordon (" + vehicles.size() + " st).");
+        return allVehicles;
     }
 
     @Override
     public void remove(String plate){
         vehicles.remove(plate);
+        logger.logInfo("Tog bort fordon med reg.nr: " + plate);
     }
 
     @Override
     public List<Vehicle> findAll(){
-        return new ArrayList<>(vehicles.values());
+        List<Vehicle> allVehicles = new ArrayList<>(vehicles.values());
+        logger.logInfo("Hämtade alla fordon (" + vehicles.size() + " st).");
+        return allVehicles;
     }
 
     public boolean exists(String plate){
